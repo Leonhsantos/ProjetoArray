a2f78a38cf0dfc9ef587998990f9244bfe6a2754
Leonardo <146380163+Leonhsantos@users.noreply.github.com>
2023-10-08 12:52:56 -0300

    Add files via upload

---
+++ projetoArray1.py
@@ -0,0 +1,13 @@
+# Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print().
+# O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc).
+# O segundo array deve conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear cada variável,
+# e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+
+
+
+prod_loja_musica = ["disco", "guitarra", "bateria", "pedal", "ukulele"]
+print(prod_loja_musica)
+
+
+data_niver = ["Alex 24/06/1999", "Caio 01/09/1997", "Gabriel 17/03/1999", "Jimmy 24/04/1999", "Gusta 18/01/2000"  ]
+print(data_niver)
